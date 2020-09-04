8e2f2670bf3e826479f23de9ba3766be56d4c39a
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-09-04 15:04:26 +0200

    Created function to update chart on resize

--- wwwroot/public/js/utilities.js
+++ wwwroot/public/js/utilities.js
@@ -11,3 +11,26 @@ closePopup = () => {
     const popup = document.getElementById('popup1')
     popup.classList.remove('showPopup')
 }
+getData = () =>{
+    return fetch(`./soy-content-vega-spec.json`)
+     .then(function(response) {
+       return response.json()
+     })
+        .then(function (json) {
+            json.encoding.color.legend.orient = 'bottom';
+       return json
+     });
+};
+   
+resetChart = () => {
+    window.addEventListener('resize', resetChart);
+    const vegaEmbed = window.vegaEmbed;
+    if (window.innerWidth <= 420) {
+            getData().then(res => {
+                vegaEmbed("#vis", res)
+            })
+    } else {
+        vegaEmbed("#vis", "soy-content-vega-spec.json")
+        .catch(console.error);
+    }
+}
