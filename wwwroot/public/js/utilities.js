13d893376a0b554c89bd19511241729cdb4ecf9c
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-09-08 23:15:17 +0200

    Added MiniPreview for all the reference links

--- wwwroot/public/js/utilities.js
+++ wwwroot/public/js/utilities.js
@@ -1,36 +1,23 @@
-showPopup = (url,content) =>{
-    const popup = document.getElementById('popup1');
-    const url_element = document.getElementById('url');
-    url_element.href = url;
-    !content &&( url_element.innerHTML = url);
-    popup.classList.add('showPopup')
-    
-}
-
-closePopup = () => {
-    const popup = document.getElementById('popup1')
-    popup.classList.remove('showPopup')
-}
-getData = () =>{
+getData = () => {
     return fetch(`./soy-content-vega-spec.json`)
-     .then(function(response) {
-       return response.json()
-     })
+        .then(function (response) {
+            return response.json()
+        })
         .then(function (json) {
             json.encoding.color.legend.orient = 'bottom';
-       return json
-     });
+            return json
+        });
 };
-   
-resetChart = () => {
-    window.addEventListener('resize', resetChart);
+
+resetChart = async () => {
+    const res = await getData();
     const vegaEmbed = window.vegaEmbed;
-    if (window.innerWidth <= 420) {
-            getData().then(res => {
-                vegaEmbed("#vis", res)
-            })
-    } else {
-        vegaEmbed("#vis", "soy-content-vega-spec.json")
-        .catch(console.error);
-    }
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 420) {
+            vegaEmbed("#vis", res)
+        } else {
+            vegaEmbed("#vis", "soy-content-vega-spec.json")
+                .catch(console.error);
+        }
+    });
 }
